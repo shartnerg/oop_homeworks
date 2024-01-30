7dec4d3c8de8a02998a55df3b9df87faf55d9c73
George <shartner@inbox.ru>
2024-01-30 19:42:58 +0200

    first

--- Seminar01_Homework/src/main/clients/Dog.java
+++ Seminar01_Homework/src/main/clients/Dog.java
@@ -4,10 +4,11 @@ import java.time.LocalDate;
 
 public class Dog extends Animal {
     public Dog(String nickName, Owner owner, LocalDate birthDate, Illness illness) {
-        super(nickName, owner, birthDate, illness); //Сходи в род.класс и вызови 4 параметра
+        super(nickName, owner, birthDate, illness); // Сходи в род.класс и вызови 4 параметра
     }
 
     public Dog(){
         super();
     }
 }
+//тест
